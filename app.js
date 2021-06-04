3183a6a368db650a79f2e9678f6d0564dc07e5ca
Naomi Ware <nware1066@yahoo.com>
2021-06-03 18:16:55 -0600

    Add switch statement to display names of days

--- app.js
+++ app.js
@@ -5,22 +5,44 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     // res.render(views/list)
 
     let today = new Date();
     var currentDay = today.getDay();
     let day = "";
 
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday"
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default: 
+        console.log("Error: current day is equal to:" + currentDay)
     }
-    
-    res.render("list", {typeOfDay: day})
+
+    res.render("list", {
+        typeOfDay: day
+    })
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Server running on port 3000")
 });
