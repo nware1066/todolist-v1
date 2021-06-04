5794092e1310490d17d7e9446a653268a3b83e78
Naomi Ware <nware1066@yahoo.com>
2021-06-03 18:45:53 -0600

    Refactor with toLocaleDateString instead of switch statement

--- app.js
+++ app.js
@@ -6,37 +6,16 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
-    // res.render(views/list)
 
     let today = new Date();
-    var currentDay = today.getDay();
-    let day = "";
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default: 
-        console.log("Error: current day is equal to:" + currentDay)
+   
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
+
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
         typeOfDay: day
